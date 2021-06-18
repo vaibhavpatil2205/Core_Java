aba16ceb99754eeaccb6e3c5a061de918eddca4f
unknown <vaibhavpatil5471@gmail.com>
2021-06-18 15:20:12 +0100

    Finding Prime Factor

---
+++ PrimeFactor.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.lang.Math;
+
+class PrimeFactor
+{
+
+    public static void primeFactors(int n)
+    {
+
+        while (n%2==0)
+        {
+            System.out.print(2 + " ");
+            n /= 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i+= 2)
+        {
+
+            while (n%i == 0)
+            {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+
+        if (n > 2)
+            System.out.print(n);
+    }
+
+    public static void main (String[] args)
+    {
+        int n = 315;
+        primeFactors(n);
+    }
+}
